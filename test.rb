540f48ed7f9aebad65aabf6e0fbcdec844ca7b81
gregoriusavip <71865513+gregoriusavip@users.noreply.github.com>
2024-09-05 18:58:51 -0700

    Change tests

--- test.rb
+++ test.rb
@@ -2,14 +2,18 @@
 
 require_relative('lib/bst')
 
-test_tree = Tree.new([1, 7, 4, 23, 8, 9, 4, 3, 5, 7, 9, 67, 6345, 324])
-test_tree.insert(10_000)
-test_tree.insert(10_001)
-test_tree.insert(10_000)
-test_tree.insert(10_002)
-test_tree.pretty_print
+test_tree = Tree.new((Array.new(15) { rand(1..100) }))
+p test_tree.balanced?
 p test_tree.inorder
+p test_tree.postorder
+p test_tree.preorder
+5.times { test_tree.insert(rand(100..1000)) }
 p test_tree.balanced?
 test_tree.rebalance
-p test_tree.inorder
 p test_tree.balanced?
+test_tree.inorder { |node| print "#{node.data} " }
+puts
+test_tree.postorder { |node| print "#{node.data} " }
+puts
+test_tree.preorder { |node| print "#{node.data} " }
+puts
