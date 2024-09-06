b5f5fac409b1eef47efba82a5ea64ef9313d0d99
gregoriusavip <71865513+gregoriusavip@users.noreply.github.com>
2024-09-05 18:36:50 -0700

    Add tests

--- test.rb
+++ test.rb
@@ -3,3 +3,13 @@
 require_relative('lib/bst')
 
 test_tree = Tree.new([1, 7, 4, 23, 8, 9, 4, 3, 5, 7, 9, 67, 6345, 324])
+test_tree.insert(10_000)
+test_tree.insert(10_001)
+test_tree.insert(10_000)
+test_tree.insert(10_002)
+test_tree.pretty_print
+p test_tree.inorder
+p test_tree.balanced?
+test_tree.rebalance
+p test_tree.inorder
+p test_tree.balanced?
